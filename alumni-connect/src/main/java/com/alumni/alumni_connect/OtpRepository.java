c200c6e0e7e9dad0dcc72294aa49e62760d9d4c0
vinuthna <enjoycoding2811@gmail.com>
2026-09-14 21:10:38 +0530

    Add OtpRepository interface for OTP management

--- alumni-connect/src/main/java/com/alumni/alumni_connect/OtpRepository.java
+++ alumni-connect/src/main/java/com/alumni/alumni_connect/OtpRepository.java
@@ -1,1 +1,16 @@
 
+package com.alumni.alumni_connect;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface OtpRepository
+        extends JpaRepository<Otp, Long> {
+
+    Optional<Otp> findTopByEmailOrderByIdDesc(
+            String email
+    );
+
+    void deleteByEmail(String email);
+}
